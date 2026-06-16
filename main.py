6206670408c22f65ff27f7e8834ba345c0cd2961
WaffySharmin <sharminmollik12@gmail.com>
2026-06-15 20:14:42 -0700

    Bank Account Management System developed using Python and Object-Oriented Programming (OOP).
    The system provides a menu-driven interface that allows users to perform basic banking operations such as creating a new account, depositing money, withdrawing money, transferring funds, viewing account details, displaying all accounts, and deleting accounts.

---
+++ main.py
@@ -0,0 +1,8 @@
+#Runner file
+from menu_bar import Menu   # menu_bar.py to class import
+
+# object create
+main_menu = Menu("x")
+
+# method call
+main_menu.main_menu()
---
+++ menu_bar.py
@@ -0,0 +1,30 @@
+#Only class (Logic/Structure)
+class Menu:
+    def __init__(self, dev_name):
+        self.name = dev_name
+    
+
+   
+    def heading(Self):
+        print(f"{'-'*60}")
+        print(f"{'#'*21} BANK ACCOUNT MANAGEMENT SYSTEM {'#'*21}")
+        print(f"{'#'*21} Developed by- Waffy {'#'*21}")
+        print(f"{'-'*60}")
+
+    def main_menu(self):
+        self.heading()
+
+        print("\n--------- MAIN MENU ---------")
+        print("1. Create New Account ")
+        print("2. Deposit Money")
+        print("3. Withdraw Money ")
+        print("4. Transfer Money ")
+        print("4. View Account Details ")
+        print("6. Display All Accounts ")
+        print("7. Delete Account ")
+        print("8. Exit ")
+#object create
+main_menu = Menu("Waffy")
+
+#method call
+main_menu.main_menu()
